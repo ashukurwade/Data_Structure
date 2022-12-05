7b646bd545f205c517ed83e93a28652197da5c8d
Ashutosh <ashutoshkurwade@gmail.com>
2022-12-05 23:22:13 +0530

    program to reverse an array or string

---
+++ array/reverseArray.py
@@ -0,0 +1,14 @@
+# program to reverse an array
+
+# Function to reverse A[] from start to end
+def reverseList(A, start, end):
+	while start < end:
+		A[start], A[end] = A[end], A[start]
+		start += 1
+		end -= 1
+
+A = [1, 2, 3, 4, 5, 6]
+print(A)
+reverseList(A, 0, 5)
+print("Reversed list is")
+print(A)
